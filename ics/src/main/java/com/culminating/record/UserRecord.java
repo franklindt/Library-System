65e9aac3aebef72afa5ffa49694ee2c6479b62fc
unknown <fr4nklinszhang@gmail.com>
2022-05-02 00:25:46 -0400

    fixed multiple classes, by adding default constructors and toStrings, added a driver test code, renamed Record to LibraryRecord due to confilicting names etc

---
+++ ics/src/main/java/com/culminating/record/UserRecord.java
@@ -0,0 +1,79 @@
+package com.culminating.record;
+
+import java.util.Date;
+
+import com.culminating.media.Media;
+import com.culminating.user.User;
+import com.culminating.payments.Fee;
+import com.culminating.utils.Log;
+
+public class UserRecord {
+    private User owner;
+    private Fee[] pastFees;
+    private Log[][] borrowHistory;
+    
+    public UserRecord() {
+      this.owner = new User();
+      this.pastFees = new Fee[1];
+      this.pastFees[0] = new Fee();
+      this.borrowHistory = new Log[1][1];
+      this.borrowHistory[0][0] = new Log();
+    }
+
+    public UserRecord(User owner, Fee[] pastFees, Log[][] borrowHistory) {
+        this.owner = owner;
+        this.pastFees = pastFees;
+        this.borrowHistory = borrowHistory;
+    }
+
+    public void appendFeeLog(Fee fee) {
+        // TODO: Implement this
+    }
+
+    public void appendBorrowHistory(Media media, Date date) {
+        // TODO: Implement this
+    }
+
+    /*
+     * Accessors and Mutators
+     */
+     
+     public User getOwner() {
+      return this.owner;
+     }
+     
+     public void setOwner(User owner) {
+      this.owner = owner;
+     }
+    public Fee[] getPastFees() {
+        return this.pastFees;
+    }
+
+    public void setPastFees(Fee[] pastFees) {
+        this.pastFees = pastFees;
+    }
+
+    public Log[][] getBorrowHistory() {
+        return this.borrowHistory;
+    }
+
+    public void setBorrowHistory(Log[][] borrowHistory) {
+        this.borrowHistory = borrowHistory;
+    }
+
+    public String toString() {
+      String ret = "";
+      ret += "User: " + this.owner.toString() + "\nFees: ";
+      for (int i = 0; i < this.pastFees.length; i++) {
+         ret += pastFees[i].toString() + ",";
+      }
+      for (int i = 0; i < this.borrowHistory.length; i++) {
+         for (int j = 0; j < this.borrowHistory[i].length; j++) {
+            ret += this.borrowHistory[i][j].toString() + ",";
+         }
+      }
+      ret = ret.substring(0, ret.length()-1);
+      return ret;
+    }
+
+}
---
+++ ics/src/main/java/com/culminating/utils/Log.java
@@ -0,0 +1,64 @@
+package com.culminating.utils;
+
+import java.util.Date;
+
+import com.culminating.media.Media;
+import com.culminating.user.User;
+
+public class Log {
+    private User user;
+    private Media item;
+    private Date date;
+    private String detail;
+    
+    public Log() {
+      this.user = new User();
+      this.item = new Media();
+      this.date = new Date();
+      this.detail = "";
+    }
+
+    public Log(User user, Media item, Date date, String detail) {
+        this.user = user;
+        this.item = item;
+        this.date = date;
+        this.detail = detail;
+    }
+
+    public User getUser() {
+        return this.user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public Media getItem() {
+        return this.item;
+    }
+
+    public void setItem(Media item) {
+        this.item = item;
+    }
+
+    public Date getDate() {
+        return this.date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public String getDetail() {
+        return this.detail;
+    }
+
+    public void setDetail(String detail) {
+        this.detail = detail;
+    }
+    
+    public String toString() {
+      return "User: " + this.user.toString() + "\nItem: " + this.item.toString() + "\nDate: " + this.date + "\nDescription: " + this.detail;
+    }
+
+}
