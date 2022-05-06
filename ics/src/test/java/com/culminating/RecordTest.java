96da25216ebddc4ec711813b44bbf7cd3e907be8
Mike Liang <liang.mike.to@gmail.com>
2022-05-05 22:15:55 -0400

    start on record tests

--- ics/src/test/java/com/culminating/RecordTest.java
+++ ics/src/test/java/com/culminating/RecordTest.java
@@ -1,5 +1,62 @@
 package com.culminating;
 
+import java.util.ArrayList;
+import java.util.Date;
+
+import com.culminating.media.Media;
+import com.culminating.payment.Fee;
+import com.culminating.record.LibraryRecord;
+import com.culminating.record.MediaRecord;
+import com.culminating.record.UserRecord;
+import com.culminating.user.User;
+import com.culminating.utils.Log;
+
+import org.junit.Test;
+
+/**
+ * 
+ */
 public class RecordTest {
+
+        User testUser = new User("Jane Smith", "109 Royal Road", 24, "female", new Date(2000, 11, 21));
+
+        Fee testFee = new Fee(testUser,
+                        new Media[] {
+                                        new Media(new Date(1942, 4, 21), "Mr. Lee", 9328, "en-ca", "random house",
+                                                        Character.valueOf('E'),
+                                                        "Comedy") },
+                        67.32, new Date(2022, 4, 21), "Overdue", false);
+
+        Log testLog = new Log(testUser,
+                        new Media(new Date(1942, 4, 21), "Mr. Lee", 9328, "en-ca", "random house",
+                                        Character.valueOf('E'),
+                                        "Comedy"),
+                        new Date(2022, 4, 21), "checkout");
+        Log testLog2 = new Log(testUser,
+                        new Media(new Date(1942, 4, 21), "Mr. Lee", 9328, "en-ca", "random house",
+                                        Character.valueOf('E'),
+                                        "Comedy"),
+                        new Date(1942, 4, 21), "checkout");
+
+        ArrayList<Log> testLogs = new ArrayList<>();
+        ArrayList<Fee> testPastFees = new ArrayList<>();
+
+        UserRecord testUserRecord = new UserRecord(testUser, testPastFees, testLogs);
+        MediaRecord testMediaRecord = new MediaRecord(testLogs,
+                        new Media(new Date(1942, 4, 21), "Mr. Lee", 9328, "en-ca", "random house",
+                                        Character.valueOf('E'),
+                                        "Comedy"));
+        LibraryRecord testLibraryRecord = new LibraryRecord(new UserRecord[] { testUserRecord },
+                        new MediaRecord[] { testMediaRecord });
+
+        /*****************
+         * Test LibraryRecord
+         *****************/
+        @Test
+        public void testRetrieval() {
+                testLogs.add(testLog);
+                testLogs.add(testLog2);
+                testPastFees.add(testFee);
+        }
 
 }
