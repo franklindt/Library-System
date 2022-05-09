00f8abd97519e8753ab1ef94c535063beb76f382
1MoonlightShadow1 <91137694+1MoonlightShadow1@users.noreply.github.com>
2022-05-08 21:42:31 -0400

    Update EBook.java

--- ics/src/main/java/com/culminating/media/EBook.java
+++ ics/src/main/java/com/culminating/media/EBook.java
@@ -1,18 +1,42 @@
+/**
+*Name(s): Franklin, Mike, Grace, Sophia
+*Date: 2022-05-04
+*Description: E-book class
+*/
+
+//packages the E-book in com.culminating.media
 package com.culminating.media;
 
+//imports Date
 import java.util.Date;
 
 public class EBook extends Book {
     public EBook() {
-        super();
+        super(); //inherits the default constructor of book
     }
 
-    public EBook(final Date publishDate, final String author, final int SIN, final String language,
-            final String publisher, final Character ageRating, final String genre, final int pages, final int isbn) {
+    /**
+    *Constructor an EBook: sets publishDate, author, SIN, language, publisher, ageRating, genre, pages, and isbn from parameters
+    *@param publishDate, the publish date of the EBook
+    *@param author, the name of the author who wrote the EBook
+    *@param SIN, the unique identity of the EBook
+    *@param language, the language the EBook is written in
+    *@param publisher, the Ebook's publisher's name
+    *@param ageRating, the age rating of the EBook
+    *@param genre, the genre of the EBook
+    *@param pages, the number of pages the EBook has
+    *@param isbn, the thirteen digit code of the EBook 
+    */
+    public EBook(Date publishDate, String author, int SIN, String language,
+            String publisher, Character ageRating, String genre, int pages, int isbn) {
         super(publishDate, author, SIN, language, publisher, ageRating, genre, pages, isbn);
     }
-
+    
     @Override
+    
+    /**
+    *Description: prints all the attributes of the EBook
+    */
     public String toString() {
         return "{" +
                 " publishDate='" + getPublishDate() + "'" +
