a6c677992d0757512d7360578f4a644d6b2a9cfd
Mike Liang <liang.mike.to@gmail.com>
2022-05-09 09:28:42 -0400

    finish transaction

--- ics/src/main/java/com/culminating/payment/Transaction.java
+++ ics/src/main/java/com/culminating/payment/Transaction.java
@@ -1,26 +1,53 @@
+/**
+*Name(s): Franklin, Mike, Grace, Sophia
+*Date: 2022-05-04
+*Description: transaction class
+*/
 package com.culminating.payment;
 
 import java.util.ArrayList;
 
 public class Transaction {
+	/** Transactions made of a list of individual Fees */
 	private ArrayList<Fee> fees;
 
+	/**
+	 * Creates a transaction with a log of Fee objects
+	 * 
+	 * @see Fee
+	 */
 	public Transaction() {
 		this.fees = new ArrayList<Fee>();
 	}
 
+	/**
+	 * Creates a Fee in the Transaction log 
+	 * 
+	 * @param fee -> a Fee object
+	 */
 	public void createFee(Fee fee) {
 		fees.add(fee);
 	}
 
+	/**
+	 * Gets the transactions
+	 * @return -> a the list of Transactions
+	 */
 	public ArrayList<Fee> getFees() {
 		return this.fees;
 	}
 
+	/**
+	 * Sets the transactions of Fees
+	 * @param fees
+	 */
 	public void setFees(ArrayList<Fee> fees) {
 		this.fees = fees;
 	}
 
+	/**
+	 * @return -> string representation of this class, shows all the Fee objects in the transaction list
+	 */
 	@Override
 	public String toString() {
 		String allFees = "";
